e197e0a2c5fec953d29fe95d91b4c21aa2ebd452
Maarten Pennings <30388931+maarten-pennings@users.noreply.github.com>
2026-02-12 16:37:43 +0100

    cleanup

--- checksum/setup.bat
+++ checksum/setup.bat
@@ -5,19 +5,15 @@ REM Set the PYTHONDIR to path for python.exe (must end in \)
 SET PYTHONDIR=C:\Programs\Python\
 IF NOT EXIST %PYTHONDIR%python.exe (
   ECHO No python.exe in %PYTHONDIR%
-  ECHO Patch line 4 in %~f0
+  ECHO Patch line 5 in %~f0
   EXIT /b
 )
 
 ECHO Creating virtual Python environment
-REM Create new private "virtual python environment"
 %PYTHONDIR%python.exe -m venv env
-REM Activate the new environment
 CALL env\Scripts\activate.bat
-REM From new Python env, upgrade pip, quietly
 ECHO Upgrading pip
 python -m pip install -q --upgrade pip setuptools wheel
-REM Add python packages to new environment
 ECHO Adding packages
 IF EXIST requirements.txt (
    pip install -q -r requirements.txt
