13561b3e72338548453f6c162dbe9dbb4cfde5bb
Maarten Pennings <30388931+maarten-pennings@users.noreply.github.com>
2026-02-12 15:41:06 +0100

    updated setup

--- checksum/setup.bat
+++ checksum/setup.bat
@@ -1,13 +1,25 @@
 @ECHO OFF
+REM setup.bat template
 
-REM If Python is not in your PATH set the PYTHONDIR manually (must end in \)
+REM Set the PYTHONDIR to path for python.exe (must end in \)
 SET PYTHONDIR=C:\Programs\Python\
+IF NOT EXIST %PYTHONDIR%python.exe (
+  ECHO No python.exe in %PYTHONDIR%
+  ECHO Patch line 4 in %~f0
+  EXIT /b
+)
 
 ECHO Found Python; creating virtual env
+REM Create new private "virtual python environment"
 %PYTHONDIR%python.exe -m venv env
+REM Activate the new environment
 CALL env\Scripts\activate.bat
-env\Scripts\python -m pip install --upgrade pip setuptools wheel
+REM From new Python env, upgrade pip, quietly
+python -m pip install -q --upgrade pip setuptools wheel
+REM Add python packages to new environment
 IF EXIST requirements.txt (
-   pip install -r requirements.txt
+   pip install -q -r requirements.txt
 )
-ECHO 'setup' done, now 'run'
+
+ECHO.'setup.bat' done, now 'run.bat'
+
